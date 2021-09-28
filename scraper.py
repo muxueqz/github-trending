da3d76e34681a82a96b726715e6202a87c73136e
muxueqz <zhangmingyuan240@gmail.com>
2021-09-28 10:57:53 +0800

    support types of since

--- scraper.py
+++ scraper.py
@@ -23,7 +23,7 @@ def createMarkdown(date, filename):
         f.write("## " + date + "\n")
 
 
-def scrape(language, filename):
+def scrape(language, since):
     HEADERS = {
         'User-Agent'		: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0',
         'Accept'			: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
@@ -31,44 +31,54 @@ def scrape(language, filename):
         'Accept-Language'	: 'zh-CN,zh;q=0.8'
     }
 
-    url = 'https://github.com/trending/{language}'.format(language=language)
+    url = 'https://github.com/trending/{language}?since={since}'.format(language=language, since=since)
     r = requests.get(url, headers=HEADERS)
     assert r.status_code == 200
     
     d = pq(r.content)
     items = d('div.Box article.Box-row')
 
+    result = set()
     # codecs to solve the problem utf-8 codec like chinese
-    with codecs.open(filename, "a", "utf-8") as f:
-        f.write('\n#### {language}\n'.format(language=language))
 
-        for item in items:
-            i = pq(item)
-            title = i(".lh-condensed a").text()
-            owner = i(".lh-condensed span.text-normal").text()
-            description = i("p.col-9").text()
-            url = i(".lh-condensed a").attr("href")
-            url = "https://github.com" + url
-            # ownerImg = i("p.repo-list-meta a img").attr("src")
-            # print(ownerImg)
-            f.write(u"* [{title}]({url}):{description}\n".format(title=title, url=url, description=description))
+    for item in items:
+        i = pq(item)
+        title = i(".lh-condensed a").text()
+        owner = i(".lh-condensed span.text-normal").text()
+        description = i("p.col-9").text()
+        url = i(".lh-condensed a").attr("href")
+        url = "https://github.com" + url
+        # ownerImg = i("p.repo-list-meta a img").attr("src")
+        # print(ownerImg)
+        result.add(u"* [{title}]({url}):{description}\n".format(title=title, url=url, description=description))
+    return result
 
 
 def job():
     strdate = datetime.datetime.now().strftime('%Y-%m-%d')
-    filename = '{date}.md'.format(date=strdate)
+    filename = 'trending.md'.format(date=strdate)
 
     # create markdown file
     createMarkdown(strdate, filename)
 
-    # write markdown
-    scrape('python', filename)
-    scrape('swift', filename)
-    scrape('javascript', filename)
-    scrape('go', filename)
+    r = {}
+    for lang in "python go javascript nim lua c".split(" "):
+    # for lang in "python".split(" "):
+        tset = set()
+        # for since in "daily".split(" "):
+        for since in "monthly weekly daily".split(" "):
+            print(lang, since)
+            tset.update(scrape(lang, since))
+            r[lang] = tset
+    from pprint import pprint
+    pprint(r)
 
-    # git add commit push
-    # git_add_commit_push(strdate, filename)
+    with open(filename, 'w') as f:
+        f.write("## " + strdate + "\n")
+        for language, v in r.items():
+            f.write('\n#### {language}\n'.format(language=language))
+            for i in v:
+                f.write(i)
 
 
 if __name__ == '__main__':
