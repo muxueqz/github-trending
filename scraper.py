56eb71ff609f95bd0d2af3119cff20a1b11987f3
muxueqz <zhangmingyuan240@gmail.com>
2021-12-28 16:02:26 +0800

    use mirrors to fix network problem

--- scraper.py
+++ scraper.py
@@ -1,11 +1,12 @@
 # coding:utf-8
 
+import marshal
 import datetime
-import codecs
 import requests
 import os
-import time
+from time import sleep, time
 from pyquery import PyQuery as pq
+from random import randrange
 
 
 def git_add_commit_push(date, filename):
@@ -22,6 +23,11 @@ def createMarkdown(date, filename):
     with open(filename, 'w') as f:
         f.write("## " + date + "\n")
 
+# github_site = 'https://github.com'
+github_sites = ['https://github.com.cnpmjs.org',
+    'https://hub.fastgit.org'
+]
+sleep_time = 3
 
 def scrape(language, since):
     HEADERS = {
@@ -30,9 +36,13 @@ def scrape(language, since):
         'Accept-Encoding'	: 'gzip,deflate,sdch',
         'Accept-Language'	: 'zh-CN,zh;q=0.8'
     }
+    random_nodeid = randrange(len(github_sites))
+    github_site = github_sites[random_nodeid]
 
-    url = 'https://github.com/trending/{language}?since={since}'.format(language=language, since=since)
+    url = '{github_site}/trending/{language}?since={since}'.format(
+        language=language, since=since, github_site=github_site)
     r = requests.get(url, headers=HEADERS)
+    print(github_site, r.status_code)
     assert r.status_code == 200
     
     d = pq(r.content)
@@ -47,7 +57,8 @@ def scrape(language, since):
         owner = i(".lh-condensed span.text-normal").text()
         description = i("p.col-9").text()
         url = i(".lh-condensed a").attr("href")
-        url = "https://github.com" + url
+        # url = github_site + url
+        url = 'https://github.com' + url
         # ownerImg = i("p.repo-list-meta a img").attr("src")
         # print(ownerImg)
         result.add(u"* [{title}]({url}):{description}\n".format(title=title, url=url, description=description))
@@ -58,27 +69,46 @@ def job():
     strdate = datetime.datetime.now().strftime('%Y-%m-%d')
     filename = 'trending.md'.format(date=strdate)
 
-    # create markdown file
-    createMarkdown(strdate, filename)
+    cache_file = './scraper.cache'
+    try:
+        with open(cache_file, 'rb') as _fd:
+            cache = marshal.load(_fd)
+    except:
+        cache = {}
+    now_timestamp = time()
 
+    langs = "python go javascript nim lua c c++ coffeescript crystal cython rust shell zig swift".split(" ")
+    # langs = "coffeescript crystal cython rust shell zig".split(" ")
     r = {}
-    for lang in "python go javascript nim lua c".split(" "):
-    # for lang in "python".split(" "):
-        tset = set()
-        # for since in "daily".split(" "):
-        for since in "monthly weekly daily".split(" "):
-            print(lang, since)
-            tset.update(scrape(lang, since))
-            r[lang] = tset
+    # create markdown file
+    with open(filename, 'a') as f:
+        f.write("## " + strdate + "\n")
+
+        for lang in langs:
+            tset = set()
+            for since in "monthly weekly daily".split(" "):
+                print(lang, since)
+                count = 0
+                while count < 10:
+                    try:
+                        tset.update(scrape(lang, since))
+                        count = 666
+                    except:
+                        pass
+                    count += 1
+
+                r[lang] = tset
+            f.write('\n#### {language}\n'.format(language=lang))
+            for i in tset:
+                if now_timestamp - cache.get(i, 0) > 60 * 60 * 24 * 30:
+                    f.write(i)
+                    cache[i] = now_timestamp
+            f.flush()
+            sleep(sleep_time)
     from pprint import pprint
     pprint(r)
-
-    with open(filename, 'w') as f:
-        f.write("## " + strdate + "\n")
-        for language, v in r.items():
-            f.write('\n#### {language}\n'.format(language=language))
-            for i in v:
-                f.write(i)
+    with open(cache_file, 'wb') as _fd:
+        marshal.dump(cache, _fd)
 
 
 if __name__ == '__main__':
